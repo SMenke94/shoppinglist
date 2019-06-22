6eee0f9556ce9619e330af320df1780c24a48303
Saskia Menke <menkesaskia@web.de>
2019-06-22 15:11:45 +0200

    Adapt env variables

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
+  :user_name => ENV['USERNAME'],
+  :password => ENV['PASSWORD'],
   :domain => 'archipinion.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
